2f98a8757f76251d7f0d11a4fb6a9514bafb783c
RajneeshPython <rajneeshkmr668@gmail.com>
2021-07-08 10:59:27 +0530

    Authentication Commit

--- core/views.py
+++ core/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from core.forms import SignUpForm
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
+
+@login_required
 def customer_page(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
+
+@login_required
 def courier_page(request):
-    return render(request, 'home.html')
-    
+    return render(request, "home.html")
+
+
+def sign_up(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+
+            login(request, user)
+            return redirect("/")
+
+    return render(request, "sign_up.html", {"form": form})
--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.contrib import auth
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
+    path('sign-up/', views.sign_up),
+
     path('', views.home),
     path('customer/', views.customer_page),
     path('courier/', views.courier_page),
