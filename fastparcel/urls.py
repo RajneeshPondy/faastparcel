2708f1ccbe80130f6c3c47195c899a4df0ce50fe
RajneeshPython <rajneeshkmr668@gmail.com>
2021-07-08 15:03:55 +0530

    Authentication (Social Auth and Send Email)

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from django.contrib import auth
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
-    path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
-    path('sign-up/', views.sign_up),
-
-    path('', views.home),
-    path('customer/', views.customer_page),
-    path('courier/', views.courier_page),
+    path("admin/", admin.site.urls),
+    path("", include("social_django.urls", namespace="social")),
+    path("sign-in/", auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path("sign-out/", auth_views.LogoutView.as_view(next_page="/")),
+    path("sign-up/", views.sign_up),
+    path("", views.home),
+    path("customer/", views.customer_page),
+    path("courier/", views.courier_page),
 ]
