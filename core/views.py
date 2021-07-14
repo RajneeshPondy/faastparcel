cfad57ec8e03344da477ee939cf625c528779c90
RajneeshPython <rajneeshkmr668@gmail.com>
2021-07-14 16:38:09 +0530

    Firebase Phone Verification

--- core/views.py
+++ core/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
 
 from core.forms import SignUpForm
 
 
 def home(request):
-    return render(request, "home.html")
-
-
-@login_required
-def customer_page(request):
-    return render(request, "home.html")
-
-
-@login_required
-def courier_page(request):
     return render(request, "home.html")
 
 
--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.contrib import auth
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib.auth import views as auth_views
 
 from core import views
+from core.customer import views as customer_views
+from core.courier import views as courier_views
+
+
+customer_urlpatters = [
+    path("", customer_views.home, name="home"),
+    path("profile/", customer_views.profile_page, name="profile"),
+]
+
+courier_urlpatters = [
+    path("", courier_views.home, name="home"),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,6 +27,9 @@ urlpatterns = [
     path("sign-out/", auth_views.LogoutView.as_view(next_page="/")),
     path("sign-up/", views.sign_up),
     path("", views.home),
-    path("customer/", views.customer_page),
-    path("courier/", views.courier_page),
+    path("customer/", include((customer_urlpatters, "customer"))),
+    path("courier/", include((courier_urlpatters, "courier"))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
